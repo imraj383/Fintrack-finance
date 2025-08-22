3eeba449d27d8ac9e9692aacb2d9944cc57eb9bb
Raj <rajpriyanshu383@gmail.com>
2025-08-22 09:52:58 +0530

    eureka client added

--- src/main/java/com/fintrack/fintrack/FintrackApplication.java
+++ src/main/java/com/fintrack/fintrack/FintrackApplication.java
@@ -2,8 +2,11 @@ package com.fintrack.fintrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FintrackApplication {
 
 	public static void main(String[] args) {
